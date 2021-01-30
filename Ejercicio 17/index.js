538937d491e9cdc99cff7779624b42e23e9ed173
rodela1 <rodela1@gmail.com>
2021-01-30 09:56:45 +0100

    Ejercicio 17-18

--- Ejercicio 17/index.js
+++ Ejercicio 17/index.js
@@ -27,26 +27,28 @@ class factura {
           total:0,
           formaPago: "confirming 240"
         };
-
+        
         this.calculo = function() {
             for (var i = 0; i <this.elementos.length; i++) {
                 this.informacion.baseImponible += this.elementos[i].cantidad * this.elementos[i].precio;
             }
              this.informacion.total = this.informacion.baseImponible * (1+(this.informacion.iva/100));
         };
-
+        
         this.muestra = function () {
             this.calculo();
             alert("Total = " + this.informacion.total + " euros");
         };
+        
+    };
 };
 
 //Proceso de instaciación de las clases
 var cliente1 = new cliente("cliente1", "", "", "");
-//var element1 = new elemento ("elemento1","1","5");
-//var element2 = new elemento ("elemento2","2","12");
-//var elements = [element1,element2];
-//var bild = new factura(user,elements);
+var element1 = new elemento ("elemento1","1","5");
+var element2 = new elemento ("elemento2","2","12");
+var elements = [element1,element2];
+var bild = new factura(cliente1,elements);
 
 //Ejecución del método Muestra
-//bild.muestra()
+bild.muestra()
